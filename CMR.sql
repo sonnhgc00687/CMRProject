d9fc1ea43d4e782b4cddbc87957902aabaa85e69
SonNguyen <sonnhgc00687@fpt.edu.vn>
2016-02-29 21:37:08 +0700

    - Add faculty in database

--- CMR.sql
+++ CMR.sql
@@ -11,14 +11,31 @@ role int
 )
 --0.Admin
 --1.Course Leader
---2.Course Moderator 
+--2.Course Moderator
+--3.PVC
+--4.DLT 
 insert into tblEmployee values('sonnguyen','123456','Son Nguyen', 0)
 insert into tblEmployee values('mainghia','123456','Mai Nghia', 1)
 insert into tblEmployee values('sondao','123456','Son Dao', 2)
+insert into tblEmployee values('ducphuc','123456','Duc Phuc', 3)
+insert into tblEmployee values('hoangha','123456','hoangha', 4)
+go
+create table tblFaculty
+(
+faculty_code nvarchar(8) primary key,
+faculty_title nvarchar(50),
+faculty_pvc nvarchar(50) references tblEmployee(username),
+faculty_dlt nvarchar(50) references tblEmployee(username),
+[start_date] date,
+[end_date] date,
+[status] int
+)
+insert into tblFaculty values('FPT2016', 'Honours Programme', 'ducphuc', 'hoangha','2016-01-01', '2016-12-31', 1)
 go
 create table tblCourse
 (
 course_code nvarchar(8) primary key,
+course_faculty nvarchar(8) references tblFaculty(faculty_code),
 course_title nvarchar(50),
 course_leader nvarchar(50) references tblEmployee(username),
 course_mod nvarchar(50) references tblEmployee(username),
@@ -26,8 +43,8 @@ course_mod nvarchar(50) references tblEmployee(username),
 [end_date] date,
 [status] int,
 )
-insert into tblCourse values('COMP1640', 'Enterprise Web Software Development', 'mainghia', 'sondao','2016-01-16', '2016-05-04', 1)
-insert into tblCourse values('COMP1649', 'Interaction Design', 'mainghia', 'sondao', '2016-01-15', '2016-05-06', 1)
+insert into tblCourse values('COMP1640', 'FPT2016', 'Enterprise Web Software Development', 'mainghia', 'sondao','2016-01-16', '2016-05-04', 1)
+insert into tblCourse values('COMP1649', 'FPT2016', 'Interaction Design', 'mainghia', 'sondao', '2016-01-15', '2016-05-06', 1)
 go
 create table tblCMR
 (
