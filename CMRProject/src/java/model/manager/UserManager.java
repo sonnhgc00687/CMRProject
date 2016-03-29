e520d1c6292e7915b5f5e8c33030954869cffb7f
Phuc <ducphuc9555@gmail.com>
2016-03-29 13:48:02 +0700

    backend edit, search, delete user

--- CMRProject/src/java/model/manager/UserManager.java
+++ CMRProject/src/java/model/manager/UserManager.java
@@ -19,6 +19,7 @@ import model.entity.User;
 public class UserManager {
 
     List<User> userList = new ArrayList<>();
+    List<User> userSearch = new ArrayList<>();
 
     public User checkUser(String username, String pass) {
         boolean status = false;
@@ -59,8 +60,8 @@ public class UserManager {
         }
         return userList;
     }
-    
-    public void createUser(String username, String password, String fullname, String email, int role){
+
+    public void createUser(String username, String password, String fullname, String email, int role) {
         SqlConnection sql = new SqlConnection();
         try {
             Connection conn = sql.connectSql();
@@ -72,6 +73,55 @@ public class UserManager {
             ps.setString(3, fullname);
             ps.setString(4, email);
             ps.setInt(5, role);
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void editUser(String username, String password, String fullname, String email, int role) {
+        SqlConnection sql = new SqlConnection();
+        try {
+            Connection conn = sql.connectSql();
+            EncryptPassword encrypt = new EncryptPassword();
+            String encryptedPassword = encrypt.encryptData(password);
+            PreparedStatement ps = conn.prepareStatement("update tblEmployee set [password]=?, fullname = ?, email=?, role=?\n"
+                    + "where username=?");
+            ps.setString(1, encryptedPassword);
+            ps.setString(2, fullname);
+            ps.setString(3, email);
+            ps.setInt(4, role);
+            ps.setString(5, username);
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public List<User> searchUserByNameOrID(String username, String fullname){
+        SqlConnection sql = new SqlConnection();
+        try {
+            Connection conn = sql.connectSql();
+            PreparedStatement ps = conn.prepareStatement("select * from tblEmployee where username = ? or fullname=?");
+            ps.setString(1, username);
+            ps.setString(2, fullname);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                User user = new User(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5));
+                userSearch.add(user);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return userSearch;
+    }
+    
+    public void deleteUserByUsername(String username){
+        SqlConnection sql = new SqlConnection();
+        try {
+            Connection conn = sql.connectSql();
+            PreparedStatement ps = conn.prepareStatement("delete tblEmployee where username = ?");
+            ps.setString(1, username);
             ps.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
