38d8d8ca68cc44c780e137b47b55c15156ed9548
Phuc95 <ducphuc9555@gmail.com>
2016-04-03 12:19:05 +0700

    Nối add account, mã hóa password khi đăng nhập

--- CMRProject/src/java/controller/GetAllUser.java
+++ CMRProject/src/java/controller/GetAllUser.java
@@ -37,19 +37,7 @@ public class GetAllUser extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet GetAllUser</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet GetAllUser at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        doGet(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -76,6 +64,10 @@ public class GetAllUser extends HttpServlet {
                 users.get(i).setRoleName("Leader");
             }else if(users.get(i).getRole() == 2){
                 users.get(i).setRoleName("Moderator");
+            }else if(users.get(i).getRole() == 3){
+                users.get(i).setRoleName("PVC");
+            }else if(users.get(i).getRole() == 4){
+                users.get(i).setRoleName("DLT");
             }
         }
         
