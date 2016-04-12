13952d1e3ecc45cb81b239d6cac66ffedc867ee3
mainghia95 <nghiamtgc00662@fpt.edu.vn>
2016-04-12 19:05:34 +0700

    Host web

--- CMRProject/src/java/model/manager/SqlConnection.java
+++ CMRProject/src/java/model/manager/SqlConnection.java
@@ -23,8 +23,9 @@ public class SqlConnection {
 //            String url = "jdbc:sqlserver://SONNGUYEN\\SQLEXPRESS:1433;databaseName=Project_CMR";
             //String url = "jdbc:sqlserver://PHUC-PC:1433;databaseName=Project_CMR";
             String url = "jdbc:sqlserver://DELL-PC:1433;databaseName=Project_CMR";
+            String hosturl = "jdbc:sqlserver://group1cmr.database.windows.net:1433;database=Project_CMR;user=mainghia@group1cmr;password=Asdf1234;encrypt=false;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
 //            String url = "jdbc:sqlserver://DELL:1433;databaseName=Project_CMR";
-            conn = DriverManager.getConnection(url, dbUser, dbPassword);
+            conn = DriverManager.getConnection(hosturl);
             return conn;
         } catch (Exception e) {
             e.printStackTrace();
